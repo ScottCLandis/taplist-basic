1110cf92ac480d940cc6e10820fea798573fd034
ScottCLandis <scott.landis@6dglobal.com>
2016-07-15 16:21:03 -0700

    alt-colors
    first attempt at alternating the color of the glass. non-functional at
    the moment.

---
+++ js/taps.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    // ID of the Google Spreadsheet
+    var spreadsheetID = "1-DGR_bflVd1tacRMHRYbNbbdNkT9rX_yK8mx17gY4Uc";
+
+    // google sheet  
+    var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+
+
+    $.get(url)
+        .done(function (data) {
+
+
+
+
+            var entry = data.feed.entry.reverse();
+
+            console.log(entry);
+            for (var i = entry.length - 1; i >= 0; i -= 1) {
+
+                var tap = entry[i]['gsx$tap']['$t'];
+                var beer = entry[i]['gsx$name']['$t'];
+                var brewery = entry[i]['gsx$brewery']['$t'];
+                var style = entry[i]['gsx$style']['$t'];
+                var abv = entry[i]['gsx$abv']['$t'];
+                var glass = entry[i]['gsx$glass']['$t'];
+
+                $(".main-contain").append("<div class=\"taps\" id=\"tap\"" + tap + "\"><div class=\"text\"><div class=\"brew-title\"><span class=\"beer\">" + beer + "</span> <span class=\"style\">" + style + "</span> </div><div class=\"info\"><span class=\"abv\">" + brewery + "</span> | <span class=\"abv\">" + abv + "% ABV</span></div>  </div> <div class=\"glass\"></div> </div>");
+                $(".glass:nth-child(odd)").append("<img class=\"resize-wide\" src=\"images/" + glass + ".png\">")
+                $(".glass:nth-child(even)").append("<img class=\"resize-wide\" src=\"images/" + glass + "-white.png\">")
+
+
+
+
+
+
+
+
+            }
+
+
+
+        });
+
+    // loading animation
+    var loading = $('.loading');
+    loading.hide();
+    $(document)
+        .ajaxStart(function () {
+            loading.show();
+        })
+        .ajaxStop(function () {
+            loading.hide();
+        });
+
+});
